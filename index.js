c66327a33cb9b9e81e0248899737575c5cf3077c
NyuB <brice.decaestecker@gmx.fr>
2023-03-02 20:06:50 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require('node:path');
+const { execSync } = require("child_process");
+
+const core = require('@actions/core');
+
+function getOptionalInput(inputKey, defaultValue) {
+    const inputValue = core.getInput(inputKey);
+    return (inputValue.length == 0) ? defaultValue : inputValue;
+}
+
+try {
+    const buildDirPath = core.getInput("build-dir");
+    const dockerFilePath = getOptionalInput("dockerfile-path", path.join(buildDirPath, "Dockerfile"));
+    const imageName = core.getInput("image-name");
+    const imageTag = core.getInput("image-tag");
+    execSync(`docker build -t ${imageName}:${imageTag} -f ${dockerFilePath} ${buildDirPath}`)
+
+} catch(error) {
+    core.setFailed(error.message);
+}
