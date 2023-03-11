907b117d9bba5d0d8e57f733b8934419608a7d0d
NyuB <brice.decaestecker@gmx.fr>
2023-03-11 19:36:28 +0100

    Integrating container-structure-test

--- index.js
+++ index.js
@@ -8,13 +8,68 @@ function getOptionalInput(inputKey, defaultValue) {
     return (inputValue.length == 0) ? defaultValue : inputValue;
 }
 
-try {
-    const buildDirPath = core.getInput("build-dir");
-    const dockerFilePath = getOptionalInput("dockerfile-path", path.join(buildDirPath, "Dockerfile"));
-    const imageName = core.getInput("image-name");
-    const imageTag = core.getInput("image-tag");
-    execSync(`docker build -t "${imageName}:${imageTag}" -f "${dockerFilePath}" "${buildDirPath}"`)
 
-} catch(error) {
-    core.setFailed(error.message);
+const containerStructureTest = {
+    linux: {
+        x64: {
+            url: "https://storage.googleapis.com/container-structure-test/v1.14.0/container-structure-test-linux-amd64",
+            binaryName: "container-structure-test-linux-amd64",
+        },
+        arm64: {
+            url: "https://storage.googleapis.com/container-structure-test/v1.14.0/container-structure-test-linux-arm64",
+            binaryName: "container-structure-test-linux-arm64"
+        }
+    },
+
+    win32: {
+        x64: {
+            url: "https://storage.googleapis.com/container-structure-test/v1.14.0/container-structure-test-windows-amd64.exe",
+            binaryName: "container-structure-test-windows-amd64.exe",
+        },
+    },
 }
+
+function chooseContainerStructureTestDistribution() {
+    const runnerOs = process.platform;
+    const runnerArchitecture = process.arch;
+    const distribution = containerStructureTest[runnerOs]?.[runnerArchitecture]
+    if (distribution === undefined) {
+        core.setFailed(`Structure test not supported for ${runnerOs} ${runnerArchitecture}`);
+    } else {
+        return distribution;
+    }
+}
+
+function testWithContainerStructureTest(configFile, imageName, imageTag) {
+    const distribution = chooseContainerStructureTestDistribution();
+    
+    if(process.platform !== "win32"){
+        execSync(`wget ${distribution.url}`);
+        execSync(`chmod +x ./${distribution.binaryName}`);
+        execSync(`./${distribution.binaryName} test --image ${imageName}:${imageTag} --config ${configFile}`);
+    } else {
+        execSync(`curl.exe --output ${distribution.binaryName} --url ${distribution.url}`)
+        execSync(`${distribution.binaryName} test --image ${imageName}:${imageTag} --config ${configFile}`);
+    }
+}
+
+async function main() {
+    try {
+        const buildDirPath = core.getInput("build-dir");
+        const dockerFilePath = getOptionalInput("dockerfile-path", path.join(buildDirPath, "Dockerfile"));
+        const imageName = core.getInput("image-name");
+        const imageTag = core.getInput("image-tag");
+        
+        execSync(`docker build -t "${imageName}:${imageTag}" -f "${dockerFilePath}" "${buildDirPath}"`);
+
+        const structureTestConfigFile = core.getInput("structure-test-file");
+        if(structureTestConfigFile.length > 0) {
+            testWithContainerStructureTest(structureTestConfigFile, imageName, imageTag);
+        }
+
+    } catch(error) {
+        core.setFailed(error.message);
+    }
+}
+
+main();
